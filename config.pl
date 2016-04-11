34e1959c273f8fc496d3ede1074708527b46bdf6
weloxux <weloxux@glowbug.nl>
2016-04-11 19:43:45 +0200

    Implement basic soy parsing

--- config.pl
+++ config.pl
@@ -2,13 +2,16 @@
 # This file defines the configuration for your wiki
 
 # Look and feel
-use constant NMS_NAME => 'An nms wiki'; # The name your wiki will use to refer to itself
-use constant NMS_THEME => 'wiki'; # The default theme. Included by default: wiki, darkwiki, book.
+use constant NMS_NAME => 'An nms wiki';	# The name your wiki will use to refer to itself
+use constant NMS_THEME => 'wiki';	# The default theme. Included by default: none yet. TODO
+use constant NMS_MAIN => 'home';	# The default landing page for your wiki
 
 # Files
-use constant FILE_IMG => '../img'; # Location for image files
-use constant FILE_JS => '../js'; # Location for JavaScript files
-use constant FILE_PAGES => '../pages'; # Location for wiki pages
-use constant FILE_LANGUAGE => 'soy'; # Markup language to be used. nms knows soy and markdown.
+use constant FILE_IMG => 'img';		# Location for image files
+use constant FILE_JS => 'js';		# Location for JavaScript files
+use constant FILE_PAGES => 'pages';	# Location for wiki pages (soy, Markdown or HTML)
+use constant FILE_OUT => 'res';		# Location for compiled pages (HTML)
+
+use constant FILE_LANGUAGE => 'soy';	# Markup language to be used. Possible options are soy, markdown and html
 
 1;
--- src/nms.pl
+++ src/nms.pl
@@ -1,26 +1,34 @@
 #!/usr/bin/perl
 # nms is a content management system, similar to a wiki
 
-#use warnings;
+use strict;
+use warnings;
 use autodie;
+
+use Switch;
+
+use Data::Dumper; #DEBUG
 
 use lib '.';
 BEGIN { require 'config.pl' };
 
-my @html = ("<!doctype html>\n<html>\n<head>\n<title>", "</title>\n<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />\n</head><body><div id=\"wrapper\">", "</div></body>\n</html>");
+my @html = ("<!doctype html><html><head><title>", "</title><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" /></head><body><div id=\"wrapper\">", "</div></body></html>");
+my $extension;
 
 sub getextension {
 	if (FILE_LANGUAGE eq 'soy') {
-		my $extension = '.soy';
+		$extension = '.soy';
 	} elsif (FILE_LANGUAGE eq 'markdown') {
-		my $extension = '.md';
-	} else { print "Unknown markup language specified! Exiting!\n"; exit; }
+		$extension = '.md';
+	} elsif (FILE_LANGUAGE eq 'html') {
+		$extension = '.html';
+	} else { die "Unknown markup language specified! Exiting!\n"; }
 }
 
 
 sub checkpages { # Check if necessary files exist
 	if (!-e FILE_PAGES) { die "No page folder found! Exiting!\n"; }
-	elsif ((!-f FILE_PAGES . "/Home" . $extension)) { die "No home page found! Exiting!\n"; }
+	elsif ((!-f FILE_PAGES . "/home" . $extension)) { die "No home page found! Exiting!\n"; }
 }
 
 sub getpages { # Get a list of all existing pages
@@ -32,36 +40,62 @@ sub getpages { # Get a list of all existing pages
 
 sub build { # Generate html for every page
 	foreach my $item (@_) {
-		makepage($item);
+		if ($item =~ /\.soy/ ) { # FIX ME FIX ME FIX ME
+			makepage($item);
+		} elsif (not $item eq '.' || $item eq '..') {
+			print "File '$item' does not seem to be in " . FILE_LANGUAGE . " format. Skipping!\n";
+		}
 	}
 }
 
 sub makepage { # Build a page
 	if (!($_[0] eq ".") and !($_[0] eq "..")) {
 		print "Building page ${_[0]}\n";
-		open(my $page, "<", FILE_PAGES . "/${_[0]}") or die "Error reading file ${_[0]}! Exiting! \n";
-		my $page = <$page>;
+		my $page = do {
+			local $/ = undef;
+			open my $fh, "<", FILE_PAGES . "/${_[0]}" or die "Could not open file! Exiting!";
+			<$fh>;
+		};
 
-		if (FILE_LANGUAGE eq 'soy') {
-			my $html = soy2html($page);
-		} elsif (FILE_LANGUAGE eq 'markdown') {
-			my $html = md2html($page);
+		switch(FILE_LANGUAGE) {
+			case 'soy'	{ writefile(soy2html($page, $_[0]), $_[0]); }
+			case 'markdown' { writefile(md2html($page, $_[0]), $_[0]); }
+			case 'html'	{ writefile(html2html($page, $_[0]), $_[0]); }
 		}
-
-		writefile($html, $_[0]);
 	}
 }
 
 sub soy2html { # Convert soy files to HTML
-	my $soy = $_[0];
+	my $content = $_[0];
+	(my $title = ${_[1]}) =~ s{\.[^.]+$}{};
+
+	for($content) {
+		s{/(.*)/}{<i>$1</i>}g;
+		s{_(.*)_}{<b>$1</b>}g;
+		s{"(.*)"}{&ldquo;<i>$1</i>&rdquo;}g;
+
+		s{\*\*\*(.*)\*\*\*}{<h3>$1</h3>}g; # headers
+		s{\*\*(.*)\*\*}{<h2>$1</h2>}g;
+		s{\*(.*)\*}{<h1>$1</h1>}g;
+	}
+
+	my $result = $html[0] . $title . $html[1] . $content . $html[2];
+	return $result;
 }
 
 sub md2html { # Convert (a subset of) markdown to HTML
-	my $md = $_[0];
+	my $content = $_[0];
+}
+
+sub html2html { # Finish up user-written HTML
+
 }
 
 sub writefile {
-	open(my $outpage, ">", FILE_PAGES . "/${_[1]}") or die "Error writing to file ${_[1]}! Exiting!\n";
+	#print "Writing file ${_[1]}. Contents: ${_[0]}\n";
+	(my $outfile = FILE_OUT . '/' . ${_[1]}) =~ s{\.[^.]+$}{\.html}; # Remove extension
+
+	open(my $outpage, ">", $outfile) or die "Error writing to file ${_[1]}! Exiting!\n";
 	print $outpage $_[0];
 	close($outpage);
 }
